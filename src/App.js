f21b764d3062844120b2e22dad5a510d91f7268f
amellab-a11y <81511767+amellab-a11y@users.noreply.github.com>
2021-04-14 14:42:58 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import React,{ Component } from 'react';
+import ImgSrcm from './img/profil.png';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+ 
+constructor(props){
+super(props);
+this.state = {
+FullName:" ",
+Bio:" ",
+Profession:" ",
+Shows: true,
+Seconds: 1 };
 }
+componentDidMount() {
+  this.timer = setInterval(() => {
+    this.setState({ Seconds: this.state.Seconds + 1 });
+  }, 1000);
+}
+
+Afficher = () => {
+  if (this.state.Shows==true){
+    this.setState({FullName: "Nadine"});
+    this.setState({Bio: "MyBio"});
+    this.setState({Profession: "MyProfession"});
+    this.setState({ImgSrcm});
+        } else {
+    alert ("la valeur de shows est false !");
+
+    };
+ 
+  }
+  
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+
+
+ render() { 
+   return (
+
+<div className="App">
+   <h1>Number of seconds is {this.state.Seconds}</h1>
+   <h1> {this.state.FullName}</h1>
+   <h1> {this.state.Bio}</h1>
+   <h1> {this.state.Profession}</h1>
+   <img src={this.state.ImgSrcm}/>
+   <h1>{this.state.Shows}</h1>
+   <button type="button" onClick={this.Afficher}> Cliquez ici pour Afficher</button>
+
+
+    </div>
+    );
+
+ }
+  
+}
+
 
 export default App;
